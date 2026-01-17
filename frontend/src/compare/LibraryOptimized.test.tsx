26242579d18a088b2d6d38db07df4eb4584ec5d2
Erokin <1107891366@qq.com>
2026-01-17 18:26:34 +0000

    test: wrap library optimized with i18n

--- frontend/src/compare/LibraryOptimized.test.tsx
+++ frontend/src/compare/LibraryOptimized.test.tsx
@@ -1,11 +1,14 @@
 import { render, screen } from '@testing-library/react'
+import { I18nProvider } from '../components/I18nProvider'
 import LibraryOptimized from './LibraryOptimized'
 
 const noop = () => {}
 
 test('renders optimized library hero', () => {
   render(
-    <LibraryOptimized localItems={[]} onImport={noop} onRemove={noop} />
+    <I18nProvider>
+      <LibraryOptimized localItems={[]} onImport={noop} onRemove={noop} />
+    </I18nProvider>
   )
 
   expect(screen.getByText('Curate your quiet library.')).toBeInTheDocument()
